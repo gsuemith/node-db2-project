93d0c43b9e6205088c6beaf304f8cff952ceae09
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-23 05:46:57 -0400

    model and router complete

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,21 @@
-const checkCarId = (req, res, next) => {
+const checkCarId = Cars => (req, res, next) => {
   // DO YOUR MAGIC
+  const carId = req.params.id
+  Cars.getById(carId)
+  .then(car => {
+    if (car) {
+      req.car = car;
+      next()
+    } else {
+      res.status(404).json({
+        message: `car with id ${carId} is not found`
+      })
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    next(err)
+  })
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +28,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,28 @@
 // DO YOUR MAGIC
+const router = require('express').Router();
+
+const Cars = require('./cars-model')
+
+router.get('/', (req, res, next) => {
+  Cars.getAll()
+  .then(cars => {
+    if (cars) {
+      res.status(200).json(cars)
+    } else {
+      res.status(404).json({
+        message: "Cars not found"
+      })
+    }
+  })
+  .catch(err => next(err))
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
+  res.status(500).json({
+    message: 'something went wrong inside the cars router',
+    errMessage: err.message,
+  })
+})
+
+module.exports = router;
